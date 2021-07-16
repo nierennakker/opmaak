4df9ca9d790011a24a90650f3860c960267551b5
nierennakker <39164644+nierennakker@users.noreply.github.com>
2021-07-16 22:52:47 +0200

    Add hotbar renderer

--- src/main/java/dev/nierennakker/opmaak/components/HotbarComponent.java
+++ src/main/java/dev/nierennakker/opmaak/components/HotbarComponent.java
@@ -1,12 +1,16 @@
 package dev.nierennakker.opmaak.components;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
-import dev.nierennakker.opmaak.Opmaak;
+import com.mojang.blaze3d.systems.RenderSystem;
 import dev.nierennakker.opmaak.api.IComponent;
 import dev.nierennakker.opmaak.api.IOpmaakAPI;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.AbstractGui;
+import net.minecraft.client.gui.IngameGui;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.ResourceLocation;
 
-public class HotbarComponent implements IComponent {
+public class HotbarComponent extends AbstractGui implements IComponent {
     @Override
     public ResourceLocation getID() {
         return new ResourceLocation(IOpmaakAPI.MOD_ID, "hotbar");
@@ -14,6 +18,31 @@ public class HotbarComponent implements IComponent {
 
     @Override
     public void render(MatrixStack stack, int x, int y, float delta) {
-        Opmaak.LOGGER.info("hotbar?");
+        Minecraft mc = Minecraft.getInstance();
+        PlayerEntity player = mc.gui.getCameraPlayer();
+
+        if (player == null) {
+            return;
+        }
+
+        RenderSystem.color4f(1.0f, 1.0f, 1.0f, 1.0f);
+        RenderSystem.enableRescaleNormal();
+        RenderSystem.enableBlend();
+        RenderSystem.defaultBlendFunc();
+
+        mc.getTextureManager().bind(IngameGui.WIDGETS_LOCATION);
+
+        this.blit(stack, x, y, 0, 0, 182, 22);
+        this.blit(stack, x - 1 + player.inventory.selected * 20, y - 1, 0, 22, 24, 22);
+
+        for (int i = 0; i < 9; i++) {
+            int offsetX = x + i * 20 + 3;
+            int offsetY = y + 3;
+
+            mc.gui.renderSlot(offsetX, offsetY, delta, player, player.inventory.items.get(i));
+        }
+
+        RenderSystem.disableRescaleNormal();
+        RenderSystem.disableBlend();
     }
 }
